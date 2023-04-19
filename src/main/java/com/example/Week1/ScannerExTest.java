4d7ae1c4a570a744c008559d5f028c004ee714a2
GangminRYOU <dbrkdals1994@gmail.com>
2023-04-19 15:47:42 +0900

    feat: 알고리즘 문제 제출 -CodeUp Commit

--- src/main/java/com/example/Week1/ScannerExTest.java
+++ src/main/java/com/example/Week1/ScannerExTest.java
@@ -1,9 +1,12 @@
 package com.example.Week1;
 
+import java.io.InputStreamReader;
+
 public class ScannerExTest {
 
     public static void main(String[] args) {
         ScannerEx scannerEx = new ScannerEx();
         scannerEx.readTwoNumbersAndPlus();
+        InputStreamReader is = new InputStreamReader(System.in);
     }
 }

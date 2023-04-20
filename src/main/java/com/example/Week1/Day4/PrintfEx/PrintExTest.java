151e747e1fbe45b0e2fc78a4fdf5748e57a8db0f
GangminRYOU <dbrkdals1994@gmail.com>
2023-04-20 11:21:47 +0900

    feat: Printf실습 - printfTest메소드 추가

---
+++ src/main/java/com/example/Week1/Day4/PrintfEx/PrintExTest.java
@@ -0,0 +1,12 @@
+package com.example.Week1.Day4.PrintfEx;
+
+public class PrintExTest {
+    static void printfTest(){
+        System.out.printf("%s\n", "Hello");
+        System.out.printf("%s\n", "bye");
+    }
+    public static void main(String[] args) {
+       PrintExTest.printfTest();
+    }
+}
+

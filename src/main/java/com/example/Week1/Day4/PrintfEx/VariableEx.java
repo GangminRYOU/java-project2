e0639ce4288d298bdf118f795ec34931b7dacbea
GangminRYOU <dbrkdals1994@gmail.com>
2023-04-20 15:22:39 +0900

    feat: Variable 선언 - VarEx 클래스 추가

---
+++ src/main/java/com/example/Week1/Day4/PrintfEx/VariableEx.java
@@ -0,0 +1,12 @@
+package com.example.Week1.Day4.PrintfEx;
+import com.example.PrintHello;
+public class VariableEx {
+    public static void main(String[] args) {
+        int iVal;
+        iVal = 0;
+        PrintHello printHello;
+        printHello = new PrintHello();
+        System.out.println(iVal);
+        printHello.print();
+    }
+}

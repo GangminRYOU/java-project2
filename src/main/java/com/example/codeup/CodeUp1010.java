550ae353e954e71c1b7417bf79085683f143072c
GangminRYOU <dbrkdals1994@gmail.com>
2023-04-20 09:53:38 +0900

    feat: 코드업 1010까지 - CodeUp1010 source code commit

---
+++ src/main/java/com/example/codeup/CodeUp1010.java
@@ -0,0 +1,9 @@
+package com.example.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1010 {
+    public static void main(String[] args) {
+        System.out.println(new Scanner(System.in).nextInt());
+    }
+}

5e43f7019f2447ce78baac354d3e0514052c85f2
GangminRYOU <dbrkdals1994@gmail.com>
2023-04-24 10:08:06 +0900

    feat: Day1 printf로 여러 타입 출력 - new Day1_1 class

---
+++ src/main/java/com/example/Week2/Day1_1.java
@@ -0,0 +1,10 @@
+package com.example.Week2;
+
+public class Day1_1 {
+    public static void main(String[] args) {
+        String sOne = new String("1");
+        int iOne = 1;
+        float fOne = 1.0f;
+        System.out.printf("sOne: %s, iOne: %d, fOne: %f", sOne, iOne, fOne);
+    }
+}
